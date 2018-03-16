557b3dbebf3505156ea28aacb10f87d227ea1cd5
Nikita <udaan.nikitapaliwal@gmail.com>
2018-03-16 14:53:04 +0530

    Create project in Studio. Create MyView for easy debugging

---
+++ app/src/main/java/udaantechb/com/getstoragefiles/ActivityHome.java
@@ -0,0 +1,33 @@
+package udaantechb.com.getstoragefiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import udaantechb.com.mylittlelibrary.MyView;
+
+public class ActivityHome extends AppCompatActivity {
+
+    Button btnClickMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        View v = new MyView(this);
+        setContentView(v);
+
+        btnClickMe=(Button)findViewById(udaantechb.com.mylittlelibrary.R.id.btnClickMe);
+
+        btnClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ActivityHome.this, "you ClickMe", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ mylittlelibrary/src/main/java/udaantechb/com/mylittlelibrary/MyView.java
@@ -0,0 +1,33 @@
+package udaantechb.com.mylittlelibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Admin on 3/15/2018.
+ */
+
+public class MyView extends LinearLayout
+{
+     private void initialize(Context context)
+     {
+         inflate(context, R.layout.my_view, this);
+     }
+
+     public MyView(Context context)
+     {
+        super(context);
+         initialize(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        initialize(context);
+    }
+}
